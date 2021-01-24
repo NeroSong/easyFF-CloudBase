b75bd8a9081984a7d3467796124099ec391dc2c0
NeroSong <hucnero@hotmail.com>
2021-01-24 23:44:55 +0800

    fix bugs

--- README.md
+++ README.md
@@ -8,14 +8,15 @@
 
 ## 简介
 
-一堆音乐需要批量修改？用户上传的录像需要做些简单处理？
+一堆视频需要批量截取？用户上传的录像需要做些简单处理？
 使用 esayFF，只需在后台设置参数，即可获得拥有以下能力的云函数：
 
-- 视频截图 / 截取片段
-- 添加 bgm / 水印
-- 拼接两段音乐
-- 给录像添加字幕
-- …… (以及任何 FFmpeg 能够实现的功能！)
+- 指定位置截图 / 截取片段
+- 短视频转换为 GIF 动图
+- 添加水印文字 / logo 图片
+- 视频消音 / 添加 bgm / 提取背景音乐
+- 音乐转换为带封面视频
+- …… (更多功能正在添加中，后续将支持手动指定 FFmpeg 命令！)
 
 可直接加入现有的业务流程，也可在后台手动生成下载。
 
@@ -34,17 +35,24 @@
 - 数据库：记录配置项和 API 调用量
 - 云函数：实现具体音视频编辑，渲染和导出功能
 - 云存储：用于储存上传和处理完毕的音视频文件
-- _后台页面_：添加修改功能配置，查看使用情况
+- _后台页面_：查看功能配置，使用情况，设置监控
 
 部署完成之后，首先在`web`目录拉起后台：
 
 ```bash
-yarn build && yarn serve
+yarn install && yarn serve
 ```
 
-点击新增按钮，添加一组功能配置:
+如果能看到当前的环境 ID，说明部署成功：
 
-生成供调用的云函数参数，可在云开发各 SDK 中使用:
+选一个规则，点击最右端的测试:
+
+选择文件上传（可使用 test 目录中提供的文件），设定参数：
+
+
+等待处理完成后下载到本地，查看是否成功：
+
+如果一切顺利，表中的处理规则可在云开发各 SDK 中使用了。
 
 例如在 Flutter 中：
 
@@ -67,19 +75,11 @@ print(res.data)
 // { ret: 0, msg : "easyFF/finish/xxx.mp4" }
 ```
 
-生成后的文件会储存在云储存中的 `easyFF` 目录:
-
-也可在功能列表中点击调用:
-
-选择文件上传，等待处理完成后下载到本地查看：
-
 ## 部署
 
 本项目基于腾讯开源项目 [CloudBase Framework](https://github.com/Tencent/cloudbase-framework) [![star](https://img.shields.io/github/stars/Tencent/cloudbase-framework?style=social)](https://github.com/Tencent/cloudbase-framework) 开发部署，支持一键云端部署。
 
-
 [![](https://main.qcloudimg.com/raw/67f5a389f1ac6f3b4d04c7256438e44f.svg)](https://console.cloud.tencent.com/tcb/env/index?action=CreateAndDeployCloudBaseProject&appUrl=https%3A%2F%2Fgithub.com%2FNeroSong%2FeasyFF-CloudBase&branch=master)
-
 
 ## 开发
 
@@ -96,7 +96,7 @@ easyFF 还在初始阶段，欢迎您的建议和反馈 :)
 0. 由于采用云端实时计算的方式实现，目前仅适用于短视频（建议为一分钟内，1080P 及以下分辨率），否则容易发生超时返回失败的情况。
 1. 因为后台页面不面向用户，且 CloudBase Framework 目前只有匿名登录的鉴权插件，放在公网不方便控制权限，所以当前仅在本地执行，不要进行静态页面托管对外使用。
 2. 生成的音视频文件默认一直保存在云储存中，长期大量使用请注意清理，以免造成较高资源托管费用。
-3. 如果出现权限拒绝错误，请注意是否在`8080`端口，其它端口需要在云开发控制台中单独添加。
+3. 如果出现权限拒绝错误，请确认后台是否跑在本地`8080`端口，其它端口需要在云开发控制台单独添加。
 
 ## 文档
 
--- web/src/views/Rules.vue
+++ web/src/views/Rules.vue
@@ -46,9 +46,13 @@
         <el-tabs v-model="tab">
           <el-tab-pane label="转换规则" name="info">
             <div class="row-bg">
-              此处建立的规则，通过云函数对相应pid的调用即可实现音视频处理。
+              通过其它 SDK
+              调用云函数时，按下表确认可选参数，即可实现音视频处理。
+              <p>
+                必选参数：pid，指定处理规则；path，待处理音视频文件的云储存位置。
+              </p>
             </div>
-            <el-row type="flex">
+            <!-- <el-row type="flex">
               <el-col>
                 <el-button
                   style="margin: 0 10px 0 0;"
@@ -57,11 +61,11 @@
                   size="mini"
                   plain
                   round
-                  @click="newRule"
+                  @click="dialogAdd = true"
                   >新增</el-button
                 >
               </el-col>
-            </el-row>
+            </el-row> -->
             <el-table
               :data="tableData"
               border
@@ -76,34 +80,82 @@
             >
               <el-table-column prop="pid" label="pid" width="70">
               </el-table-column>
-              <el-table-column prop="type" label="类型" width="300">
+              <el-table-column prop="type" label="类型" width="200">
               </el-table-column>
-              <el-table-column prop="para" label="参数" width="100">
-              </el-table-column>
-              <el-table-column prop="file" label="所需文件" width="100">
+              <el-table-column prop="para" label="参数示例" width="100">
               </el-table-column>
               <el-table-column prop="note" label="说明"> </el-table-column>
-              <el-table-column fixed="right" label="操作" width="100">
+              <el-table-column fixed="right" label="操作" width="70">
                 <template slot-scope="scope">
                   <el-button
-                    @click="editRow(scope.row)"
+                    @click="checkFF(scope.row.pid)"
                     type="text"
                     size="small"
-                    >编辑</el-button
-                  >
-                  <el-button
-                    @click="checkRow(scope.row)"
-                    type="text"
-                    size="small"
-                    >调用</el-button
+                    >测试</el-button
                   >
                 </template>
               </el-table-column>
             </el-table>
+            <div class="row-bg">
+              <p>
+                调用成功后返回参数：ret和msg。ret为0，则msg为生成文件云端位置，否则msg为错误提示。
+              </p>
+              <p>后续会增加自定义 FFmpeg 命令功能。</p>
+            </div>
           </el-tab-pane>
         </el-tabs>
       </el-col>
     </el-row>
+    <el-dialog
+      title="添加转换规则"
+      width="600px"
+      :visible.sync="dialogAdd"
+      :close-on-click-modal="false"
+      :show-close="false"
+    >
+      <el-row style="margin-bottom: 10px;" type="flex" justify="start">
+        <el-col style="width:70px; margin-top: 5px;">类型：</el-col>
+        <el-select v-model="valueType" size="medium" placeholder="请选择">
+          <el-option
+            v-for="item in options"
+            :key="item.value"
+            :label="item.label"
+            :value="item.value"
+          >
+          </el-option>
+        </el-select>
+      </el-row>
+      <el-row style="margin-bottom: 10px;">
+        <el-col style="width:70px; margin-top: 5px;">说明：</el-col>
+        <el-col style="width:80%;">
+          <el-row type="flex" justify="start" style="margin-top: 5px;">
+            {{ valueAbout }}
+          </el-row>
+          <!-- <el-input
+            size="small"
+            v-model="valueAbout"
+          ></el-input> -->
+        </el-col>
+      </el-row>
+      <el-row style="margin-bottom: 10px;">
+        <el-col style="width:70px; margin-top: 5px;">参数：</el-col>
+        <el-col style="width:80%;">
+          <el-input clearable size="small" v-model="paraAdd"></el-input>
+        </el-col>
+      </el-row>
+      <span slot="footer">
+        <el-row type="flex" justify="end">
+          <span>
+            <el-button size="medium" @click="dialogAdd = false"
+              >取 消</el-button
+            >
+            <el-button size="medium" type="primary" @click="addNew"
+              >确 定</el-button
+            >
+          </span>
+        </el-row>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -120,15 +172,76 @@ export default {
       tableData: [
         {
           pid: 1001,
-          type: 1,
-          para: 1,
-          file: 2,
-          note: "about",
+          type: "视频指定位置截图",
+          para: "t: 5",
+          note: "参数为：需要截图的时间点t。以秒为单位，默认为第5秒截图",
+        },
+        {
+          pid: 1002,
+          type: "视频截取片段",
+          para: "t: 0, len: 10",
+          note:
+            "参数为：截取片段的开始时间t，持续时间len。默认为0秒开始，截取10秒",
+        },
+        {
+          pid: 1003,
+          type: "短视频转为Gif图",
+          para: "无",
+          note: "无需额外参数",
+        },
+        {
+          pid: 1004,
+          type: "视频添加文字水印",
+          para: "s: '水印'",
+          note: "参数为：水印字符串s。默认为“水印”二字",
+        },
+        {
+          pid: 1005,
+          type: "视频添加图片水印",
+          para: "p2: 'aa.jpg'",
+          note: "参数为：水印图片在云端的地址p2。在使用本规则时为必选参数。",
+        },
+        {
+          pid: 1006,
+          type: "视频消音",
+          para: "无",
+          note: "无需额外参数",
+        },
+        {
+          pid: 1007,
+          type: "视频添加Bgm",
+          para: "p2: 'xx.mp3'",
+          note: "参数为：背景音乐在云端的地址p2。在使用本规则时为必选参数。",
+        },
+        {
+          pid: 1008,
+          type: "获取视频Bgm",
+          para: "无",
+          note: "无需额外参数",
+        },
+        {
+          pid: 1009,
+          type: "音乐转为封面视频",
+          para: "p2: 'yy.jpg'",
+          note: "参数为作为视频的封面图片p2。在使用本规则时为必选参数。",
         },
       ],
+      dialogAdd: false,
     }
   },
-  methods: {},
+
+  watch: {
+    valueType: function(val) {
+      this.valueAbout = this.aboutArrs[val]
+      this.paraAdd = this.paraArrs[val]
+    },
+  },
+
+  methods: {
+    addNew() {
+      this.dialogAdd = false
+    },
+  },
 }
 </script>
 
